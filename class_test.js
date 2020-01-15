285e8fc9a0b096290a47e40167d3ce4f71af1406
Lee-Soyeon <leesoyena@gmail.com>
2020-01-15 16:18:57 +0900

    TIL

--- class_test.js
+++ class_test.js
@@ -16,7 +16,31 @@ function Clazz(msg) {
 // 객체를 생성
 var myClazz = new Clazz("good to see u!");
 console.log(myClazz.message);
+
 // this를 사용하지 않은 message2 변수는 외부에서 참조할 수 없다.
 console.log(myClazz.message2);
+
 // this로 선언된 함수를 통해 사용할 수 있다.
 myClazz.print();
+
+function Clazz(msg) {
+  this.name = "I am Class";
+  this.message = msg;
+  message2 = "find me!";
+}
+
+//Clazz 객체의 prototype 을 가져와서 message값을 리턴하는 함수를 하나 추가한다.
+Clazz.prototype.getMessage = function() {
+  return this.message;
+};
+
+Clazz.prototype.getMessage2 = function() {
+  return this.message2;
+};
+
+// 객체를 생성
+var myClazz = new Clazz("good to see u!");
+console.log(myClazz.getMessage());
+
+// 내부에 선언된 함수와는 다르게 prototype으로 선언한 함수는 값을 사용할 수없다.
+console.log(myClazz.getMessage2());
